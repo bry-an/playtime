d7a2febd2f77c1d4fff2265f32bd406befa49699
bry-an <bbyunis@gmail.com>
2018-06-18 22:27:57 -0500

    explain bindings component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class ExplainBindingsComponent extends Component {
+  onLickMe() {
+    console.log(this);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <botton
+      onClick={this.onClickMe}
+      type="button"
+      >
+      Click Me
+      </button>
     );
   }
 }
-
 export default App;
